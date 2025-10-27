d1ffe814aa8fe6c087ea09288f3efe8f313156ce
sunjianxiang <1504734652@qq.com>
2025-10-27 13:27:21 +0800

    build: 添加vite-plugin-css-injected-by-js插件并更新版本
    添加vite-plugin-css-injected-by-js插件以支持CSS注入JS功能，同时更新package.json版本号至0.1.2并修正exports字段

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cssInjectedByJs from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    cssInjectedByJs(),
   ],
   resolve: {
     alias: {
